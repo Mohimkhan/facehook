40658510dfd22113456926a9fd227e7ae5139f52
Mohimkhan <mohimkhan652@gmail.com>
2024-03-06 13:38:37 +0600

    Use private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegistrationPage from "./pages/RegistrationPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import PrivateRoute from "./routes/PrivateRoutes";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route
-          element={<HomePage />}
+          element={<PrivateRoute />}
           path="/"
-        ></Route>
+        >
+          <Route
+            element={<HomePage />}
+            path="/"
+            exact
+          />
+          <Route
+            element={<ProfilePage />}
+            path="/me"
+          />
+        </Route>
         <Route
           element={<LoginPage />}
           path="/login"
-        ></Route>
-        <Route
-          element={<ProfilePage />}
-          path="/me"
         ></Route>
         <Route
           element={<RegistrationPage />}
